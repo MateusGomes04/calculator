9c7ec0868b2960451b7db9097fa826fe3fa6542c
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-15 08:12:54 -0300

    addition of getters and setters to define and protect access to classes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,4 +7,24 @@ class CalcController {
 
     }
 
+    get displayCalc(){
+        return this._displayCalc;
+
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+
+    }
+
 }
