95afca402dc25000e0daf06f59910a166c821dbb
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-20 16:37:03 -0300

    Audio API and external resources

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,8 @@
 class CalcController {
 
     constructor() {
-
+        this._audio = new Audio("click.mp3");
+        this._audioOnOff = false;
         this._lastOperation = "";
         this._lastNumber = "";
         this._operation = [];
@@ -60,12 +61,42 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll(".btn-ac").forEach(btn=>{
+
+            btn.addEventListener("dblclick", e=>{
+
+            this.toggleAudio();
+
+            });
+        });
+
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+
+    playAudio(){
+
+        if(this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+
+            this._audio.play();
+        }
+
 
     }
 
     initKeyboard() {
 
         document.addEventListener("keyup", e => {
+
+            this.playAudio();
 
             switch (e.key) {
 
@@ -183,9 +214,18 @@ class CalcController {
 
     getResult() {
 
+        try {
 
-        return eval(this._operation.join(""));
+            return eval(this._operation.join(""));
+        } catch (e) {
 
+            setTimeout(() => {
+
+                this.setError();
+
+            }, 1);
+
+        }
 
     }
 
@@ -324,6 +364,8 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio();
 
         switch (value) {
 
