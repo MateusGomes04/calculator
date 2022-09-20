9f90584bb4a95b34a56c4186183edbd9db98a2f1
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-20 11:39:07 -0300

    limit the amount of characters on the display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -445,6 +445,15 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+
+        if (value.toString().length > 10) {
+
+            this.setError();
+            return false;
+
+        }
+
         this._displayCalcEL.innerHTML = value;
 
     }
